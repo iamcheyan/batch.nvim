640fca471194974da28c4a355165cd4983a0c4f6
cheyan <iamcheyan@gmail.com>
2026-09-19 18:12:01 +0900

    feat: modernize contextline breadcrumb segment icons

--- lua/batch/context.lua
+++ lua/batch/context.lua
@@ -5,14 +5,35 @@ function M.get_info(opts)
   if not info then
     return nil
   end
-  local segments = {
-    { text = "LABEL > " .. info.label, hl = "Identifier" },
-    { text = "LINE " .. info.line, hl = "Number" },
-  }
+  local segments = {}
+  if info.label and info.label ~= "" then
+    table.insert(segments, {
+      text = info.label,
+      label = "label",
+      icon = "󰊕",
+      icon_hl = "Function",
+      hl = "Function",
+      type = "symbol",
+    })
+  end
+  if info.line then
+    table.insert(segments, {
+      text = "L" .. info.line,
+      label = "line",
+      icon = "󰎠",
+      icon_hl = "Number",
+      hl = "Number",
+      type = "symbol",
+    })
+  end
   if info.target then
     table.insert(segments, {
       text = info.target_kind .. " :" .. info.target,
+      label = "target",
+      icon = "󰌗",
+      icon_hl = "Keyword",
       hl = "Keyword",
+      type = "symbol",
     })
   end
   return {
